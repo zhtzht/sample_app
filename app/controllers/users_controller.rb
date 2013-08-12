ffcecca0a2d15a6fde449034a8b10cf865e9545f
zht <zhtzht15@163.com>
2013-08-12 15:30:51 +0800

    Finish user edit,update,destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+
+  before_action :signed_in_user,    only:[:index,:edit,:update]
+  before_action :correct_user,   only: [:edit, :update]  
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page:params[:page])
+    
+  end
   def new
   	@user = User.new
   end
@@ -15,10 +24,38 @@ class UsersController < ApplicationController
   		sign_in @user
   	   flash[:success] = "Welcome to the Sample App!"
        redirect_to @user
+
   	else
+       flash.now[:error] = "Invalid email/password combination"
        render 'new'
   	end
 
+  end
+
+  def edit
+
+    @user = User.find(params[:id])
+    
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroy."
+    redirect_to users_url
+    
+  end
+
+  def update
+
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+       flash[:success] = "Profile updated"
+       sign_in @user
+       redirect_to @user
+    else
+       render 'edit'
+    end
+    
   end
 
   private
@@ -27,5 +64,20 @@ class UsersController < ApplicationController
 
   	params.require(:user).permit(:name,:email,:password,:password_confirmation)
   	
+  end
+  def admin_user
+      redirect_to(root_path) unless current_user.admin?
+  end
+  
+  def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
 end
